b99cc6a844e1f5b70db1b20df1df489fbf33e144
dmitryjob4j <haoos@inbox.ru>
2022-03-03 19:24:38 +0300

    3.1.6. Пулы. 1. Реализовать ThreadPool [#1099]в процессе.

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,51 @@
+package ru.job4j.pool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.waitnotify.prodcons.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 3.1.6. Пулы
+ * 1. Реализовать ThreadPool [#1099]
+ * Задание. Реализуйте требуемый в задании функционал.
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 03.03.2022
+ */
+public class ThreadPool {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ThreadPool.class.getName());
+    private static final int SIZE = Runtime.getRuntime().availableProcessors();
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
+
+    public void work(Runnable job) {
+        try {
+            tasks.offer(job);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void runTasks(SimpleBlockingQueue<Runnable> tasks) {
+    }
+
+    /**
+     * Метод shutdown() завершает все запущенные задачи.
+     */
+    public void shutdown() {
+        for (Thread thread : threads) {
+            try {
+                if (thread.getState() != Thread.State.TERMINATED) {
+                    thread.join();
+                    thread.interrupt();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                LOGGER.error("Thread interrupted", e.getCause());
+            }
+        }
+    }
+}
